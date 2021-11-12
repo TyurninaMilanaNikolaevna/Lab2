87ba846986c5d2eac7b3f0fb90debdb6a9d2f48b
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 13:00:57 +0300

    Scripted auto-commit on change (2021-11-12 13:00:57) by gitwatch.sh

--- src/main/java/lab2/FlightDelayTimeApp.java
+++ src/main/java/lab2/FlightDelayTimeApp.java
@@ -2,13 +2,14 @@ package lab2;
 
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FlightDelayTimeApp {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
         job.setJarByClass(FlightDelayTimeApp.class);
-        job.setJobNane("Flight Delay Time App");
+        job.setJobName("Flight Delay Time App");
 
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
