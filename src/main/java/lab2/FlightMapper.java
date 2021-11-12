1ee50904d0f3501e51f96a41920225c9e4a9bb1e
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 15:14:51 +0300

    Scripted auto-commit on change (2021-11-12 15:14:51) by gitwatch.sh

--- src/main/java/lab2/FlightMapper.java
+++ src/main/java/lab2/FlightMapper.java
@@ -1,6 +1,24 @@
 package lab2;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
 import java.io.IOException;
 
-public class FlightMapper {
+public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+
+    public static int AIRPORT_CODE_POSITION = 0;
+    public static int AIRPORT_DESCRIPTION_POSITION = 1;
+    public static int INDICATOR = 0;
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper <LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
+        String[] airportsCodeAndDescription = value.toString().split(",");
+        if (key.get() > 0) {
+            int airportCode = Integer.parseInt(airportsCodeAndDescription[AIRPORT_CODE_POSITION]);
+            String airportDescription = airportsCodeAndDescription[AIRPORT_DESCRIPTION_POSITION];
+            context.write(new AirportWritableComparable(airportCode, INDICATOR), new Text(airportDescription));
+        }
+    }
 }
