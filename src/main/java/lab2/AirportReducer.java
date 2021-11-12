273f043255c12f863927c776129dc582ea1e2596
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 17:31:44 +0300

    Scripted auto-commit on change (2021-11-12 17:31:44) by gitwatch.sh

--- src/main/java/lab2/AirportReducer.java
+++ src/main/java/lab2/AirportReducer.java
@@ -2,5 +2,5 @@ package lab2;
 
 public class AirportReducer {
 
-    protected void reduce(AirportWritableComparable key, )
+    protected void reduce(AirportWritableComparable key, Iterable<Text>, )
 }
