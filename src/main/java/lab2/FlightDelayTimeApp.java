ae2748d82f3425589f6f95ffbc6d1a38cf04e59a
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 12:55:37 +0300

    Scripted auto-commit on change (2021-11-12 12:55:37) by gitwatch.sh

--- src/main/java/lab2/FlightDelayTimeApp.java
+++ src/main/java/lab2/FlightDelayTimeApp.java
@@ -4,6 +4,9 @@ public class FlightDelayTimeApp {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
         job.setJarByClass(FlightDelayTimeApp.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         
+
     }
 }
