e137e360577eb96e6d263d7a5cf1eb1f907a98ef
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 14:08:25 +0300

    Scripted auto-commit on change (2021-11-12 14:08:25) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper <LongWritable, Text, , Text> {
+
+    public static AIRPORT_CODE_POSITION = 0;
     
 
 }
