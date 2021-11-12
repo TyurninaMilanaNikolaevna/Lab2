e54f873b5928ecb16e3dd930af8f8cad213587a6
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 14:11:58 +0300

    Scripted auto-commit on change (2021-11-12 14:11:58) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -4,14 +4,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper <LongWritable, Text, , Text> {
+public class AirportMapper extends Mapper <LongWritable, Text, AirportWritableComparable, Text> {
 
     public static int AIRPORT_CODE_POSITION = 0;
     public static int AIRPORT_DESCRIPTION_POSITION = 1;
 
     @Override
-    protected void map() throws IOException
+    protected void map() throws IOException, InterruptedException {
 
-
+    }
 }
 
