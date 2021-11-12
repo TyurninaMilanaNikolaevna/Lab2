44d94e4e1a47e8b88944ffb2759512662a91a116
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 15:34:14 +0300

    Scripted auto-commit on change (2021-11-12 15:34:14) by gitwatch.sh

--- src/main/java/lab2/FlightDelayTimeApp.java
+++ src/main/java/lab2/FlightDelayTimeApp.java
@@ -1,6 +1,7 @@
 package lab2;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
@@ -12,7 +13,7 @@ public class FlightDelayTimeApp {
         job.setJobName("Flight Delay Time App");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
-
+        FileOutputFormat
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
