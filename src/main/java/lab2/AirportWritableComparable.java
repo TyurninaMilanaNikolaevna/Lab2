cc4ec5947a2e6c8306fda42dcca845f8c0ab9e46
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 15:11:08 +0300

    Scripted auto-commit on change (2021-11-12 15:11:08) by gitwatch.sh

--- src/main/java/lab2/AirportWritableComparable.java
+++ src/main/java/lab2/AirportWritableComparable.java
@@ -1,6 +1,9 @@
 package lab2;
 
 public class AirportWritableComparable {
+    
     public AirportWritableComparable(int airportCode, int indicator) {
+        this.airportCode = airportCode;
+        this.indicator = indicator;
     }
 }
