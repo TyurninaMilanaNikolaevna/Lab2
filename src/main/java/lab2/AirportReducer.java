05caa13d53c4bd36db1dd6a8e4a20488ad63f1ff
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 17:36:27 +0300

    Scripted auto-commit on change (2021-11-12 17:36:27) by gitwatch.sh

--- src/main/java/lab2/AirportReducer.java
+++ src/main/java/lab2/AirportReducer.java
@@ -1,11 +1,15 @@
 package lab2;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
 import java.io.IOException;
 import java.util.Iterator;
 
 public class AirportReducer {
 
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Reducer<AirportWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        Iterator<Text> iterator
+        Iterator<Text> iterator = values.iterator();
+
     }
 }
