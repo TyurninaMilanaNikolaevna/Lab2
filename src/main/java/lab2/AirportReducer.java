027873b85c6be8bbcc71f121ef0d3346a9e46d5b
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 17:35:19 +0300

    Scripted auto-commit on change (2021-11-12 17:35:19) by gitwatch.sh

--- src/main/java/lab2/AirportReducer.java
+++ src/main/java/lab2/AirportReducer.java
@@ -1,10 +1,11 @@
 package lab2;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportReducer {
 
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Reducer<AirportWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        
+        Iterator<Text> 
     }
 }
