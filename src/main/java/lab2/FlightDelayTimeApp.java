be85253f502b14597bf33262db5ec0de6a3c8099
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 15:35:52 +0300

    Scripted auto-commit on change (2021-11-12 15:35:52) by gitwatch.sh

--- src/main/java/lab2/FlightDelayTimeApp.java
+++ src/main/java/lab2/FlightDelayTimeApp.java
@@ -14,6 +14,8 @@ public class FlightDelayTimeApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path((args[2])));
+
+        
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
