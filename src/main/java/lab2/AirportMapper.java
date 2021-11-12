634c3e55cffa504c3988f14d01f800e6398bc63a
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 15:08:06 +0300

    Scripted auto-commit on change (2021-11-12 15:08:06) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -15,7 +15,7 @@ public class AirportMapper extends Mapper <LongWritable, Text, AirportWritableCo
     @Override
     protected void map(LongWritable key, Text value, Mapper <LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         String[] airportsCodeAndDescription = value.toString().split(",");
-        if (key.get() != 0) {
+        if (key.get() > 0) {
             int airportCode = Integer.parseInt(airportsCodeAndDescription[AIRPORT_CODE_POSITION]);
             String airportDescription = airportsCodeAndDescription[AIRPORT_DESCRIPTION_POSITION];
             context.write(new AirportWritableComparable(airportCode, INDICATOR), new Text(airportDescription));
