ab436787912bf999475d7b82ac9cca68c889cebe
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-12 16:43:23 +0300

    Scripted auto-commit on change (2021-11-12 16:43:23) by gitwatch.sh

--- src/main/java/lab2/AirportPartitioner.java
+++ src/main/java/lab2/AirportPartitioner.java
@@ -1,5 +1,6 @@
 package lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
