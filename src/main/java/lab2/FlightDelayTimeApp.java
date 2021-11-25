90ecc41f6d83841468867c624a2386f74829d352
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-25 14:28:24 +0300

    Scripted auto-commit on change (2021-11-25 14:28:24) by gitwatch.sh

--- src/main/java/lab2/FlightDelayTimeApp.java
+++ src/main/java/lab2/FlightDelayTimeApp.java
@@ -3,7 +3,7 @@ package lab2;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
 
