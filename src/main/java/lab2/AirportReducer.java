346d919bdad08cd16c90234c4a56ebce507f060e
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-25 11:52:40 +0300

    Scripted auto-commit on change (2021-11-25 11:52:40) by gitwatch.sh

--- src/main/java/lab2/AirportReducer.java
+++ src/main/java/lab2/AirportReducer.java
@@ -9,6 +9,11 @@ import java.util.Iterator;
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
 
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Reducer<AirportWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+
+        float minDelayTime = Float.MAX_VALUE;
+        float maxDelayTime = 0;
+        float sumDelayTime = 0;
+
         Iterator<Text> iterator = values.iterator();
         if (iterator.hasNext()) {
             Text airportDescription = new Text(iterator.next());
