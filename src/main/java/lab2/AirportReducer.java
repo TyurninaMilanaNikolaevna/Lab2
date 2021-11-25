27efe40aca4cfa3182682c62e4906cd6efaae711
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-25 12:03:49 +0300

    Scripted auto-commit on change (2021-11-25 12:03:49) by gitwatch.sh

--- src/main/java/lab2/AirportReducer.java
+++ src/main/java/lab2/AirportReducer.java
@@ -33,8 +33,8 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
             counter++;
 
             averageDelayTime = sumDelayTime / counter;
-            
-            context.write();
+
+            context.write(airportDescription, );
         }
     }
 }
