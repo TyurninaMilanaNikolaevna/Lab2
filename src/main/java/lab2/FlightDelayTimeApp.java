55699211a445c515ce49a36fc69e2111bb18c689
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-25 13:59:04 +0300

    Scripted auto-commit on change (2021-11-25 13:59:04) by gitwatch.sh

--- src/main/java/lab2/FlightDelayTimeApp.java
+++ src/main/java/lab2/FlightDelayTimeApp.java
@@ -2,16 +2,17 @@ package lab2;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
-import org.apache.hadoop.mapreduce.Job;
 
 public class FlightDelayTimeApp {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
         job.setJarByClass(FlightDelayTimeApp.class);
         job.setJobName("Flight Delay Time App");
+
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
@@ -19,10 +20,11 @@ public class FlightDelayTimeApp {
         job.setPartitionerClass(AirportPartitioner.class);
         job.setReducerClass(AirportReducer.class);
         job.setGroupingComparatorClass(GroupingComparator.class);
+        job.setMapOutputKeyClass(AirportWritableComparable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-
         job.setNumReduceTasks(2);
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
