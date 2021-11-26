96d6914056d50c22c794576c9a903c55fddba6f7
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-26 11:50:02 +0300

    Scripted auto-commit on change (2021-11-26 11:50:02) by gitwatch.sh

--- src/main/java/lab2/FlightMapper.java
+++ src/main/java/lab2/FlightMapper.java
@@ -15,7 +15,9 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
 
     @Override
     protected void map(LongWritable key, Text value, Mapper <LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
-        String[] flightDescription = value.toString().split(SPLITTER);
+        String[] flightDescription = value.toString()
+                .replace("\"", "")
+                .split(SPLITTER);
         if (key.get() > 0) {
             int airportCode = Integer.parseInt(flightDescription[DEST_AIRPORT_ID_POSITION]);
             String flightDelay = flightDescription[ARR_DELAY_POSITION];
