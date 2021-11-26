86e518662828aa2b20908a85de864411756b847c
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-26 11:38:16 +0300

    Scripted auto-commit on change (2021-11-26 11:38:16) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -19,7 +19,7 @@ public class AirportMapper extends Mapper <LongWritable, Text, AirportWritableCo
         if (key.get() > 0) {
 
             int airportCode = Integer.parseInt(airportCodeAndDescription[AIRPORT_CODE_POSITION]);
-            String airportDescription = airportCodeAndDescription[AIRPORT_DESCRIPTION_POSITION];
+            String airportDescription = airportCodeAndDescription[AIRPORT_DESCRIPTION_POSITION].replace("\"", "");
             context.write(new AirportWritableComparable(airportCode, INDICATOR), new Text(airportDescription));
         }
     }
