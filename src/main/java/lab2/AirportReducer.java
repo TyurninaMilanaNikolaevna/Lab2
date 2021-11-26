91582b0b702d6621934596a76e85aeeb65a93fd3
TyurninaMilana <milanatyurnina@gmail.com>
2021-11-26 13:25:37 +0300

    Scripted auto-commit on change (2021-11-26 13:25:37) by gitwatch.sh

--- src/main/java/lab2/AirportReducer.java
+++ src/main/java/lab2/AirportReducer.java
@@ -5,10 +5,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.logging.Logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
-    private static Logger log = Logger.getLogger(AirportReducer.class.getName());
 
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Reducer<AirportWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
